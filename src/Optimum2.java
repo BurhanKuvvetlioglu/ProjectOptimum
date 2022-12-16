cbfb61a591b8e63fd6473cafba2342893258add4
Sinan Mutlusoy <mutlusinan2020@gmail.com>
2022-12-16 14:07:04 +0300

    Optimum2 file modified Optimum3 file created

--- src/Optimum2.java
+++ src/Optimum2.java
@@ -2,4 +2,6 @@ public class Optimum2 {
     public static void main(String[] args) {
         System.out.println("We'll practice collaboration");
     }
+
+
 }
---
+++ src/Optimum3.java
@@ -0,0 +1,6 @@
+public class Optimum3 {
+    public static void main(String[] args) {
+
+        System.out.println("we have been working on the 3rd class.");
+    }
+}
